d9037179018f157bceb8b7201cf0c073fed87314
Daniel Wall <dw268145@gmail.com>
2023-10-08 15:04:52 -0400

    Move around plugins stuff part 2

--- init.lua
+++ init.lua
@@ -20,7 +20,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup("custom.plugins")
+require('lazy').setup("plugins")
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
